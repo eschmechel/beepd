21dae44bef4d601ba18e91bbe0e7b70cb5138337
Elliott Schmechel <elliottschmechel+git@gmail.com>
2026-01-14 15:15:04 -0800

    (feat): Added web app scaffolding

---
+++ apps/web/astro.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  output: 'hybrid',
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
+  integrations: [
+    react(),
+    tailwind(),
+  ],
+  vite: {
+    resolve: {
+      alias: {
+        '@': './src',
+        '@beepd/ui': '../../packages/ui/src',
+        '@beepd/api-client': '../../packages/api-client/src',
+      },
+    },
+  },
+});
